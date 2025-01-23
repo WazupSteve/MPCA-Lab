6033e496d7c7576f557cdad068a1d77e84318fa7
Amit Prakash <amit05.prakash@gmail.com>
2025-01-23 16:39:10 +0530

    pushed docs

--- week-1/5.s
+++ week-1/5.s
@@ -1,6 +1,6 @@
-.text 
+.text
 .global _start
-
 _start:
-    MOV R0, #21 @ Load 20 into R0
-    AND R1, R0, #1  @ R1 = R0 & 1
+    MOV R0, #21 @ Load number to check
+    AND R1, R0, #1 @ R1 = R0 & 1 (remainder when divided by 2)
+    MOV R2, R1 @ Move result to R2 (0 for even, 1 for odd)
